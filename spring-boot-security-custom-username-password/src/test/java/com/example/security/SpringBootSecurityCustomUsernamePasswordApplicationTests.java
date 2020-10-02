18df77a58403711cd0eb026c3780e049559e4cc2
ANU <ANU@VAIO>
2020-10-02 17:18:58 +0530

    pushing the branch for development..

---
+++ spring-boot-security-custom-username-password/src/test/java/com/example/security/SpringBootSecurityCustomUsernamePasswordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.security;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityCustomUsernamePasswordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
